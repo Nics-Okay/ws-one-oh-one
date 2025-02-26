3cd633d834c55511bd8906358303bb22b1f08aa2
Nics <officialnicholeson@gmail.com>
2025-02-26 23:36:26 +0800

    Test only | Homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    <div>
+        <h1>Hello  Prends</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, exercitationem natus, voluptas soluta, veniam architecto consectetur labore repellendus ratione quas odit amet sit id nesciunt. Distinctio, iure? Sit, similique officia?</p>
+    </div>
+</body>
+</html>
